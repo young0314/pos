063c1aca3549642d449d79c8073004dd78177cd2
young <111072282+young0314@users.noreply.github.com>
2025-03-28 14:15:57 +0900

    컨테이너 삭제 기능 추가
    - 등록 메서드 삭제

--- AppBackend/Service/ContainerService.java
+++ AppBackend/Service/ContainerService.java
@@ -1,45 +1,49 @@
 package com.example.pos_app.Service;
 
-import com.example.pos_app.DTO.RegiContainerDTO;
+import com.example.pos_app.Model.ContainImage;
 import com.example.pos_app.Model.RegiContainer;
-import com.example.pos_app.Model.Container;
-import com.example.pos_app.Model.User;
+import com.example.pos_app.Repository.ContainImageRepository;
 import com.example.pos_app.Repository.RegiContainerRepository;
-import com.example.pos_app.Repository.UserRepository;
 import com.example.pos_app.Repository.ContainerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ContainerService {
 
-    @Autowired
-    private RegiContainerRepository regiContainerRepository;
-    @Autowired
-    private ContainerRepository containerRepository;
-    @Autowired
-    private UserRepository userRepository;
 
-    // 컨테이너 등록 메서드
-    public RegiContainer registerContainer(RegiContainerDTO containerDto, String adminId) {
-        // adminId로 User 엔티티 찾기
-        User admin = userRepository.findByAdminId(adminId).orElseThrow(() -> new IllegalArgumentException("Admin not found"));
+    private final RegiContainerRepository regiContainerRepository;
 
-        // 기존 컨테이너 찾기
-        Container container = containerRepository.findById(containerDto.getContainNumber())
-                .orElseThrow(() -> new IllegalArgumentException("Container not found"));
+    private final ContainerRepository containerRepository;
 
-        // RegiContainer 객체 생성
-        RegiContainer regiContainer = RegiContainer.builder()
-                .containNumber(containerDto.getContainNumber())
-                .destination(containerDto.getDestination())
-                .cargo(containerDto.getCargo())
-                .containerOwner(containerDto.getContainerOwner())
-                .container(container) // 기존 Container와 연결
-                .user(admin) // 관리자와 연결
-                .build();
 
-        // RegiContainer 저장
-        return regiContainerRepository.save(regiContainer);
+    private final ContainImageRepository containImageRepository;
+    public ContainerService(RegiContainerRepository regiContainerRepository,
+                            ContainerRepository containerRepository,
+                            ContainImageRepository containImageRepository){
+            this.regiContainerRepository = regiContainerRepository;
+            this.containerRepository = containerRepository;
+            this.containImageRepository = containImageRepository;
+    }
+
+    // 컨테이너 삭제 메서드
+    @Transactional
+    public void deleteByContainNumber(String containNumber) {
+        // 1. RegiContainer 삭제
+        List<RegiContainer> regiContainers = regiContainerRepository.findByContainNumber(containNumber);
+        if (!regiContainers.isEmpty()) {
+            regiContainerRepository.deleteAll(regiContainers);
+        }
+
+        // 2. ContainImage 삭제
+        List<ContainImage> containImages = containImageRepository.findAllByContainer_ContainNumber(containNumber);
+        if (!containImages.isEmpty()) {
+            containImageRepository.deleteAll(containImages);
+        }
+
+        // 3. Container 삭제
+        containerRepository.deleteById(containNumber);
     }
 }
