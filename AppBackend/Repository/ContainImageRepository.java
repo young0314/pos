d0a60c1f5d6280f9c3e1e5b7175348b02af45883
young <111072282+young0314@users.noreply.github.com>
2025-03-28 14:23:17 +0900

    리스트 형식 findAllByContainer_ContainNumber 추가

--- AppBackend/Repository/ContainImageRepository.java
+++ AppBackend/Repository/ContainImageRepository.java
@@ -1,11 +1,13 @@
 package com.example.pos_app.Repository;
 
 import com.example.pos_app.Model.ContainImage;
-import com.example.pos_app.Model.Container;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ContainImageRepository extends JpaRepository<ContainImage, Long> {
     Optional<ContainImage> findByContainer_ContainNumber(String containNumber);
+    List<ContainImage> findAllByContainer_ContainNumber(String containNumber);
+
 }
