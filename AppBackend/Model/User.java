4567f547c3165e2c6113c132d7b2a5144d8bad99
young <111072282+young0314@users.noreply.github.com>
2026-04-05 16:59:24 +0900

    adminId->adPk기본키 변경
    기본키 값 변경

--- AppBackend/Model/User.java
+++ AppBackend/Model/User.java
@@ -1,13 +1,12 @@
 package com.example.pos_app.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.Random;
 
 @Getter
 @Setter
@@ -17,12 +16,17 @@ import java.util.Random;
 @Entity
 public class User {
     @Id
-    private String adminId;
-    private String adminName;
-    private String idNumber;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adPk; //PK
+    @Column(unique = true, nullable = false)
+    private String email; //아이디로 사용
+    private boolean emailVerified; //이메일 인증 여부
+    private String idNumber; //주민번호
     private String phone;
-    private String password;
+    private String adminName; //이름
+    private String password; //비밀번호
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     @CreationTimestamp
     private LocalDateTime createdDate;
 
